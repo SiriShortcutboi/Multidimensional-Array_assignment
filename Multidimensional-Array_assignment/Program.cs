6dac1430ab128449f33c6b8c229c8fc4da63b1d3
unknown <holden.anderson@ENGINEERING.SNOW>
2026-02-04 02:39:02 -0700

    Fix globals and movement input

--- Multidimensional-Array_assignment/Program.cs
+++ Multidimensional-Array_assignment/Program.cs
@@ -1,11 +1,12 @@
 ﻿//--------------------------------- Global Variables ---------------------------------------//
-using System.Diagnostics.CodeAnalysis;
+using System;
 
 Random rand = new Random();
 string[] Challenges = { "Empty", "Monster", "Puzzle", "Trap", "Exit" };
 bool KeepPlaying = true;
 int Row;
 int Column;
+int Location;
 int[,] Dungeon = new int[5, 5]
 {
     { 0, 1, 0, 2, 0 },
@@ -15,27 +16,28 @@ int[,] Dungeon = new int[5, 5]
     { 2, 0, 1, 3, 0 }
 };
 
+//----------------------------- Pseudocode Section ---------------------------------------//{
 //explanation of how to start and build this out//
 //rought out what you can and call it good, if 2 dimesnions in your array seems
 // to work for your code thats great. otherwise go just 1 dimension
 //not a key skill
-//----- Make Choose start position--------------//
+//--- Make Choose start position--------------//
 
-//---------- After giving a starting position, loop a method every turn  ------------------//
+//------- After giving a starting position, loop a method every turn  ------------------//
 
-//---------- that moves the player around the array 0 thorugh 4 (thats 5 spaces------------------//
+//------- that moves the player around the array 0 thorugh 4 (thats 5 spaces------------------//
 
-//---------- if they try to exceed 5th spot "Must've hit a wall" and move diff direction-------------------//
+//------- if they try to exceed 5th spot "Must've hit a wall" and move diff direction-------------------//
 
-//---------- with different destinations registering a number to spawn monsters  ------------------//
+//------- with different destinations registering a number to spawn monsters  ------------------//
 
-//---------- a room registered with 0 is an empty room, nice, next turn  ------------------//
+//------- a room registered with 0 is an empty room, nice, next turn  ------------------//
 
-//----------user tries to get to the 4 spot in the middle ------------------//
+//-------user tries to get to the 4 spot in the middle ------------------//
 
+//}
 
-
-//------------------------------- Main Code that runs ---------------------------------------//
+//----------------------------- Main Code that runs ---------------------------------------//
 ChooseStartPosition();
 
 while (KeepPlaying)
@@ -62,14 +64,17 @@ void ChooseStartPosition()
                     //comment this int and make another just like it
                     //dungeon should contain random row and random column
 {                  //row first, column second
-    //int location = Dungeon[4, 2];
-    int location = Dungeon[rand.Next(0, 5), rand.Next(0, 5)];
-    if (location == 4)
+    Row = rand.Next(0, 5);
+    Column = rand.Next(0, 5);
+    Location = Dungeon[Row, Column];
+    if (Location == 4)
     {
-        location = Dungeon[rand.Next(0, 5), rand.Next(0, 5)];
+        Row = rand.Next(0, 5);
+        Column = rand.Next(0, 5);
+        Location = Dungeon[Row, Column];
     }
-    Console.WriteLine(location);
-    Console.WriteLine(Challenges[location]);
+    Console.WriteLine(Location);
+    Console.WriteLine(Challenges[Location]);
 
 
 
@@ -86,13 +91,40 @@ void Move()
     if (direction != "W" && direction != "A" && direction != "S" && direction != "D")
     {
         Console.WriteLine("Invalid input, please use the following buttons to move: W-Key UP, S-Key DOWN, A-key LEFT, D-key RIGHT");
-        direction = Console.ReadLine();
+                var key = Console.ReadKey(true).Key;
+        if (key == ConsoleKey.W) { /* up */ } direction = Console.ReadLine();
+        }
+        else if (direction == "w")
+        {
+            //move up
+            Row--;
+            Location = Dungeon[Row, Column];
+        }
+        
+        else if (direction == "a")
+        {
+            //move left
+            Column--;
+            Location = Dungeon[Row, Column];
+        }
+        else if (direction == "s")
+        {
+            //move down
+            Row++;
+            Location = Dungeon[Row, Column];
+        }
+        else if (direction == "d")
+        {
+            //move right
+            Column++;
+            Location = Dungeon[Row, Column];
+        }
     }
 
 
-}
 
 void ThankYouForPlaying()
 {
+    Console.WriteLine("You made it out of the cave! Thank you for playing!");
     //Thank you for playing message
 }
